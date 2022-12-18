94612b33223b376e9046451df00ea0933b0d0bcc
Guilhermebs <guilhermebs@drcmr.dk>
2022-12-18 09:01:33 -0300

    day18, optimized

--- day18.py
+++ day18.py
@@ -22,18 +22,14 @@ def solve():
     known_outside = set()
     known_inside = set()
     for i, empty_c in enumerate(empty_cubes):
-        if i % 1000 == 0:
-            print(f"{i/len(empty_cubes):%} completed", end="\r")
-        if empty_c in known_outside or empty_c in known_inside:
-            pass
         is_out, searched = connected_to_outside(
-            empty_c, all_cubes, bounding_box
+            empty_c, all_cubes, bounding_box, known_outside, known_inside
         )
         if is_out:
             known_outside |= searched
         else:
             known_inside |= searched
-    print("")
+
     sol_part2, _ = exposed_faces(all_cubes | known_inside)
     print("Part 2:", sol_part2)
 
@@ -45,17 +41,23 @@ def exposed_faces(all_cubes):
             for sign in [+1, -1]:
                 coords = list(cube)
                 coords[i] += sign
-                n_exposed_faces += tuple(coords) not in all_cubes
-                empty_cubes.append(tuple(coords))
+                coords = tuple(coords)
+                if coords not in all_cubes:
+                    n_exposed_faces += 1
+                    empty_cubes.append(coords)
 
     return n_exposed_faces, empty_cubes
 
 
-def connected_to_outside(empty_space, all_cubes, bounding_box):
+def connected_to_outside(empty_space, all_cubes, bounding_box, known_outside, known_inside):
     seen = set([empty_space])
     queue = deque([empty_space])
     while len(queue) > 0:
         coords = queue.popleft()
+        if coords in known_inside:
+            return False, seen
+        if coords in known_outside:
+            return True, seen
         if any(c < bbmin or c > bbmax for c, bbmin, bbmax in zip(coords, bounding_box[0], bounding_box[1])):
             return True, seen
         else:
