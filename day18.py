80ae6e22966f85bc80599492bba00ad83268d6ce
Guilhermebs <guilhermebs@drcmr.dk>
2022-12-18 08:25:47 -0300

    day18

---
+++ day18.py
@@ -0,0 +1,77 @@
+import os
+import time
+from collections import deque
+
+
+def solve():
+    input_file_contents = open(os.path.join("input", "day18")).read().rstrip()
+
+    all_cubes = set(tuple(int(c) for c in line.split(",")) for line in input_file_contents.splitlines())
+    n_exposed_faces, empty_cubes = exposed_faces(all_cubes)
+
+    sol_part1 = n_exposed_faces
+    print("Part 1:", sol_part1)
+
+    bounding_box = tuple([
+        tuple(min(c[i] for c in all_cubes) for i in range(3)),
+        tuple(max(c[i] for c in all_cubes) for i in range(3))
+    ])
+
+    # starting at an empty cube, do BFS search. If we ever go outside the
+    # bounding box, it is connected. Otherwise stop
+    known_outside = set()
+    known_inside = set()
+    for i, empty_c in enumerate(empty_cubes):
+        if i % 1000 == 0:
+            print(f"{i/len(empty_cubes):%} completed", end="\r")
+        if empty_c in known_outside or empty_c in known_inside:
+            pass
+        is_out, searched = connected_to_outside(
+            empty_c, all_cubes, bounding_box
+        )
+        if is_out:
+            known_outside |= searched
+        else:
+            known_inside |= searched
+    print("")
+    sol_part2, _ = exposed_faces(all_cubes | known_inside)
+    print("Part 2:", sol_part2)
+
+def exposed_faces(all_cubes):
+    n_exposed_faces = 0
+    empty_cubes = []
+    for cube in all_cubes:
+        for i in range(3):
+            for sign in [+1, -1]:
+                coords = list(cube)
+                coords[i] += sign
+                n_exposed_faces += tuple(coords) not in all_cubes
+                empty_cubes.append(tuple(coords))
+
+    return n_exposed_faces, empty_cubes
+
+
+def connected_to_outside(empty_space, all_cubes, bounding_box):
+    seen = set([empty_space])
+    queue = deque([empty_space])
+    while len(queue) > 0:
+        coords = queue.popleft()
+        if any(c < bbmin or c > bbmax for c, bbmin, bbmax in zip(coords, bounding_box[0], bounding_box[1])):
+            return True, seen
+        else:
+            for i in range(3):
+                for sign in (-1, 1):
+                    neighbour = list(coords)
+                    neighbour[i] += sign
+                    neighbour = tuple(neighbour)
+                    if neighbour not in all_cubes and neighbour not in seen:
+                        queue.append(neighbour)
+                        seen |= set([neighbour])
+
+    return False, seen
+
+
+if __name__ == "__main__":
+    start = time.time()
+    solve()
+    print(f"Run time: {time.time() - start:.3f}s")
