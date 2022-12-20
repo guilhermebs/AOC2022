4721922a8116f3dbb494772b52dcac97573b3e1c
Guilhermebs <guilhermebs@drcmr.dk>
2022-12-20 09:27:13 -0300

    day20

--- day20.py
+++ day20.py
@@ -5,40 +5,36 @@ import time
 def solve():
     input_file_contents = open(os.path.join("input", "day20")).read().rstrip()
     seq = [int(x) for x in input_file_contents.splitlines()]
-    seq = [1, 2, -3, 3, -2, 0, 4]
     mixed = mix(seq)
     coords = get_grove_coords(mixed)
-    print(coords)
-
     sol_part1 = sum(coords)
     print("Part 1:", sol_part1)
 
     key = 811589153
     mixed = [x*key for x in seq]
-    print(mixed)
-    for i in range(10):
-        mixed = mix(mixed)
-        print(i, mixed)
+    mixed = mix(mixed, rounds=10)
     coords = get_grove_coords(mixed)
     sol_part2 = sum(coords)
     print("Part 2:", sol_part2)
 
 
-def mix(seq):
+def mix(seq, rounds=1):
     tot_len = len(seq)
     indices = [idx for idx in range(tot_len)]
-    for i, n in enumerate(seq):
-        mix_index = indices.index(i)
-        indices.pop(mix_index) # here the length of the list decreases!
-        # also, the first and last position are the same!
-        move_to = (mix_index + n) % (tot_len - 1)
-        indices.insert(move_to, i)
-        #print(n, [seq[idx] for idx in indices])
-    
+    for _ in range(rounds):
+        for i, n in enumerate(seq):
+            mix_index = indices.index(i)
+            indices.pop(mix_index)  # here the length of the list decreases!
+            # also, the first and last position are the same!
+            move_to = (mix_index + n) % (tot_len - 1)
+            indices.insert(move_to, i)
+
     return [seq[idx] for idx in indices]
+
 
 def get_grove_coords(seq):
     return [seq[(idx + seq.index(0)) % len(seq)] for idx in (1000, 2000, 3000)]
+
 
 if __name__ == "__main__":
     start = time.time()
